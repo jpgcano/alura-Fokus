1cdc8c68710cdf5cbf9707d196101db089c224ab
juan pablo cano gallo <86212300+jpgcano@users.noreply.github.com>
2024-05-19 23:24:07 -0500

    terminando proyecto

--- script2.js
+++ script2.js
@@ -4,20 +4,53 @@ const bottonEnfoque = document.querySelector('.app__card-button--enfoque');
 const bottonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
 const titulo = document.querySelector('.app__title');
+const botones = document.querySelectorAll('.app__card-button');
+const inputEnfoqueMusica = document.querySelector('#alternar-musica');
+const bottonIniciarPausar = document.querySelector('#start-pause');
+const TextoIniciarPausar = document.querySelector('#start-pause span');
+const iconoIniciarPausar = document.querySelector(".app__card-primary-butto-icon");
+const tiempoEnPantalla = document.querySelector('#timer');
+
+const musica = new Audio('./sonidos/luna-rise-part-one.mp3');
+const musicaPause = new Audio('./sonidos/pause.mp3');
+const musicaInicio = new Audio('./sonidos/play.wav');
+const musicaFinal = new Audio('./sonidos/beep.mp3');
+
+let tiempoTranscurridoEnSengundos = 1500;
+let intervalo = null;
+
+
+inputEnfoqueMusica.addEventListener('change', () => {
+    if (musica.paused) {
+        musica.play();
+    }
+    else {
+        musica.pause();
+    }
+});
 
 bottonCorto.addEventListener('click', () => {
     cambiarContexto('descanso-corto');
+    tiempoTranscurridoEnSengundos = 300;
+    bottonCorto.classList.add('active');
 });
 bottonEnfoque.addEventListener('click', () => {
     cambiarContexto('enfoque');
+    tiempoTranscurridoEnSengundos = 1500;
+    bottonEnfoque.classList.add('active');
 });
 bottonLargo.addEventListener('click', () => {
     cambiarContexto('descanso-largo');
+    tiempoTranscurridoEnSengundos = 900;
+    bottonLargo.classList.add('active');
 });
 function cambiarContexto(contexto) {
+    mostrarTiempo();
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `/imagenes/${contexto}.png`);
-
+    botones.forEach((contexto) => {
+        contexto.classList.remove('active');
+    });
     switch (contexto) {
         case "enfoque":
             titulo.innerHTML = `Optimiza tu productividad,<br>
@@ -35,3 +68,45 @@ function cambiarContexto(contexto) {
             break;
     }
 }
+
+
+
+
+const cuentaRegresiva = () => {
+    if (tiempoTranscurridoEnSengundos <= 0) {
+        musicaFinal.play();
+        reiniciar();
+        return;
+    }
+    TextoIniciarPausar.textContent = 'Pausar';
+    iconoIniciarPausar.setAttribute('src', `/imagenes/pause.png`);
+    tiempoTranscurridoEnSengundos -= 1;
+    mostrarTiempo();
+    console.log("temporizadir: " + tiempoTranscurridoEnSengundos);
+};
+
+bottonIniciarPausar.addEventListener('click', iniciarPausar);
+
+function iniciarPausar() {
+    console.log(intervalo);
+    if (intervalo) {
+        musicaPause.play();
+        reiniciar();
+        return;
+    }
+    musicaInicio.play();
+    intervalo = setInterval(cuentaRegresiva, 1000);
+}
+function reiniciar() {
+    clearInterval(intervalo);
+    TextoIniciarPausar.textContent = 'Comenzar'
+    intervalo = null;
+}
+
+function mostrarTiempo() {
+    const tiempo = new Date(tiempoTranscurridoEnSengundos * 1000);
+    const tiempoFormateado = tiempo.toLocaleDateString('es-col', { minute: '2-digit', second: '2-digit' });
+    tiempoEnPantalla.innerHTML = `${tiempoFormateado}`
+}
+
+mostrarTiempo();
